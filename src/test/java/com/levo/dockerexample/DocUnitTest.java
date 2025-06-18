11b1bbeb8d780d86d0d8ffc4dd2ceff061152843
AniAdithya <53913984+AniAdithya@users.noreply.github.com>
2025-06-19 00:13:22 +0530

    Update DocUnitTest.java

--- src/test/java/com/levo/dockerexample/DocUnitTest.java
+++ src/test/java/com/levo/dockerexample/DocUnitTest.java
@@ -5,7 +5,7 @@ public class DocUnitTest{
  @Test
    public void testOnePlusOne() {
        assertEquals(2, 1 + 1);
-       assertEquals(3, 2 + 1);
+       assertEquals(4, 2 + 2);
    }
 
     @Test
